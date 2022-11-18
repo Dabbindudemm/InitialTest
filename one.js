e787f3f016ae1fe288c52d13f9d6a8c4521d1715
Sulav Ghimire <dabbindudemm@gmail.com>
2022-11-18 10:11:52 +0545

    InitialCommit

---
+++ one.js
@@ -0,0 +1,18 @@
+function the () {
+document.getElementById('meow').style.display="block"    
+}
+
+
+
+function functione() {
+    document.getElementById('cow').innerHTML="Boom the paragraph changed and the color as well"
+    document.getElementById('cow').style.backgroundColor="black"
+    document.getElementById('cow').style.color="white"
+    document.getElementById('uno').style.display="block"
+    }
+
+
+
+ function uhm () {
+ window.alert(" Downloading Virus.....");
+ }  
